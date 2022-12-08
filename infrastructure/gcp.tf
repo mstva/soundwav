763b2f271699615ba71bc91bfc387d4add4eff58
mstva <mustafa.abdulluh@gmail.com>
2022-12-08 15:46:08 +0200

    added gcp cloud sql resource

--- infrastructure/gcp.tf
+++ infrastructure/gcp.tf
@@ -15,11 +15,20 @@ provider "google" {
   credentials = var.gcp.credentials
 }
 
+variable "database_password" { type = string }
+
 module "gcp" {
   source = "./modules/gcp"
 
   storage_name     = "${local.project_name}-storage"
   storage_location = "US-EAST1"
+
+  database_instance_name = "${local.project_name}-database"
+  database_version       = "POSTGRES_14"
+  database_tier          = "db-f1-micro"
+  database_name          = "${local.project_name}_db"
+  database_username      = "${local.project_name}_user"
+  database_password      = var.database_password
 
 }
 
